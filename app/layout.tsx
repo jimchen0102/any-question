deda14627546efdfae86073657b351287fdec9f4
jim.chen <azx011014@gmail.com>
2024-10-06 00:19:56 +0800

    feat: refactor UI components and styles in app
    - Add ESLint rule to disable custom classnames in Tailwind for `lib/index.js`
    - Exclude binary file comparison for favicon in `app/favicon.ico`
    - Refactor global CSS styles and import custom styles from `theme.css` in `app/globals.css`
    - Implement font configuration and metadata in `app/layout.tsx` using Next.js font optimization
    - Modify the ClerkProvider component in `app/layout.tsx` to include custom styling
    - Add new assets and icons such as `account.svg`, `arrow-left.svg`, and `arrow-right.svg`
    - Adjust test utilities and configurations in `app/layout.tsx`
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk as SpaceGrotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
 import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = SpaceGrotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'AnyQuestion',
+  description: 'Ask any question',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+}
 
 export default function RootLayout({
   children,
@@ -8,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
