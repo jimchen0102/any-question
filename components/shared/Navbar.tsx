18a7c0420035e6e43b1fb0b3d9823df54926f91b
jim.chen <azx011014@gmail.com>
2024-10-06 11:38:02 +0800

    feat: refine UI styling and configuration setup
    - Remove custom `tailwindcss/no-custom-classname` eslint rule
    - Add a new `components.json` configuration file
    - Modify style class in Navbar component for consistency in shadow presentation
    - Add new utility functions for class manipulation in `lib/utils.ts`
    - Introduce several new dependencies related to UI styling in `package.json`
    - Enhance `tailwind.config.ts` with additional color, animation, and container configurations
    - Include `tailwindcss-animate` plugin in Tailwind configuration
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,7 +5,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="assets/images/site-logo.svg"
