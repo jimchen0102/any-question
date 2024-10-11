5c971c84a71110d431408f8211e4585ff727e82d
jim.chen <azx011014@gmail.com>
2024-10-11 22:29:30 +0800

    feat: enhance UI for question interactions
    - Introduce display components for question cards and no result messages
    - Implement utility functions for formatting numbers and generating timestamps
    - Develop detailed UI elements for questions, including metrics and tags
    - Add dynamic result handling and question listing on the home page
    - Summarize changes to types in shared components
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,6 +4,45 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { HomePageFilters } from '@/constants/filters'
 import HomeFilters from '@/components/home/HomeFilters'
+import NoResult from '@/components/shared/NoResult'
+import QuestionCard from '@/components/cards/QuestionCard'
+
+const questions = [
+  {
+    _id: '1',
+    title: 'Cascading Deletes in SQLAlchemy?',
+    tags: [
+      { _id: '1', name: 'python' },
+      { _id: '2', name: 'sql' },
+    ],
+    author: {
+      _id: '1',
+      name: 'John Doe',
+      picture: 'john-doe.jpg',
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date('2021-09-01T12:00:00.000Z'),
+  },
+  {
+    _id: '2',
+    title: 'How to center a div?',
+    tags: [
+      { _id: '1', name: 'css' },
+      { _id: '2', name: 'sql' },
+    ],
+    author: {
+      _id: '1',
+      name: 'John Doe',
+      picture: 'john-doe.jpg',
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date('2021-09-01T12:00:00.000Z'),
+  },
+]
 
 export default function Home() {
   return (
@@ -35,6 +74,23 @@ export default function Home() {
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard key={question._id} {...question} />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break this silence! Ask a Question and kickstart the
+        discussion. our query could be the next big thing others learn from. Get
+        involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   )
 }
