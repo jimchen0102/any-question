3d9b0688d9c39b0d858a941d55fbb2ca4b2ffddb
jim.chen <azx011014@gmail.com>
2024-10-12 12:04:02 +0800

    feat: refactor homepage question fetching logic
    - Implement API call for dynamic question retrieval in homepage
    - Replace hardcoded question list with dynamically fetched data in homepage
    - Introduce error handling in `getQuestions` api function
    - Add `pathname` parameter to the question creation process
    - Implement revalidation logic for new questions to refresh cached content
    - Define TypeScript interfaces for question parameters to ensure type safety in operations involving question data
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,45 +6,12 @@ import { HomePageFilters } from '@/constants/filters'
 import HomeFilters from '@/components/home/HomeFilters'
 import NoResult from '@/components/shared/NoResult'
 import QuestionCard from '@/components/cards/QuestionCard'
+import { getQuestions } from '@/lib/actions/question.action'
 
-const questions = [
-  {
-    _id: '1',
-    title: 'Cascading Deletes in SQLAlchemy?',
-    tags: [
-      { _id: '1', name: 'python' },
-      { _id: '2', name: 'sql' },
-    ],
-    author: {
-      _id: '1',
-      name: 'John Doe',
-      picture: 'john-doe.jpg',
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date('2021-09-01T12:00:00.000Z'),
-  },
-  {
-    _id: '2',
-    title: 'How to center a div?',
-    tags: [
-      { _id: '1', name: 'css' },
-      { _id: '2', name: 'sql' },
-    ],
-    author: {
-      _id: '1',
-      name: 'John Doe',
-      picture: 'john-doe.jpg',
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date('2021-09-01T12:00:00.000Z'),
-  },
-]
+export default async function Home() {
+  const result = await getQuestions({})
+  console.log(result.questions)
 
-export default function Home() {
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -76,9 +43,19 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
-            <QuestionCard key={question._id} {...question} />
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
           ))
         ) : (
           <NoResult
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -2,10 +2,34 @@
 
 import Question from '@/database/question.model'
 import Tag from '@/database/tag.model'
+import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
+import { GetQuestionsParams, CreateQuestionParams } from './shared.types'
+import { revalidatePath } from 'next/cache'
 
-export async function createQuestion(params: any) {
-  // eslint-disable-next-line no-empty
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase()
+
+    const questions = await Question.find({})
+      .populate({
+        path: 'tags',
+        model: Tag,
+      })
+      .populate({
+        path: 'author',
+        model: User,
+      })
+      .sort({ createdAt: -1 })
+
+    return { questions }
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase()
 
@@ -32,5 +56,10 @@ export async function createQuestion(params: any) {
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocument } },
     })
-  } catch (error) {}
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
