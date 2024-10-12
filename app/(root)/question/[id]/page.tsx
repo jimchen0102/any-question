1028cfe41e743f47c2e6ca7c9ce7bde2afe57de9
jim.chen <azx011014@gmail.com>
2024-10-12 22:53:52 +0800

    feat: refactor `Votes` component and update config
    - Enhance the `Votes` component in `page.tsx` by tracking question type, item, user IDs, upvotes/downvotes counts, and saved status
    - Rewrite the `Votes` component in `Votes.tsx` to include voting functionality, use of next/image for icons, and dynamic display of vote counts using formatted numbers
    - Remove `serverActions` from experimental features in `next.config.mjs`
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -43,7 +43,16 @@ const page = async ({ params, searchParams }: any) => {
           </Link>
 
           <div className="flex justify-end">
-            <Votes />
+            <Votes
+              type="question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser?._id)}
+              upvotes={result.upvotes.length}
+              hasUpvoted={result.upvotes.includes(mongoUser?._id)}
+              downvotes={result.downvotes.length}
+              hasDownvoted={result.downvotes.includes(mongoUser?._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
