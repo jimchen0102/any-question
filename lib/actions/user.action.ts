b239d1209b98f55708595f4f0e972592eee05e4b
jim.chen <azx011014@gmail.com>
2024-10-12 19:48:14 +0800

    feat: enhance community section user interactions
    - Add a new `page.tsx` in the community section with components for user display and search
    - Create a `UserCard` component displaying user details and tags
    - Implement the `getTopInteractedTags` function to fetch tags interacted most by a user
    - Add `getAllUsers` functionality in `user.action.ts` to fetch and sort all users by creation date
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,6 +6,7 @@ import {
   CreateUserParams,
   UpdateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
 } from './shared.types'
 import { revalidatePath } from 'next/cache'
 import Question from '@/database/question.model'
@@ -81,3 +82,29 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase()
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params
+
+    const users = await User.find({}).sort({
+      createdAt: -1,
+    })
+
+    return { users }
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase()
+//   } catch (error) {
+//     console.log(error)
+//     throw error
+//   }
+// }
