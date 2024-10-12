047e9cc60a9658e0937ca83000b5a21b9a3f2d91
jim.chen <azx011014@gmail.com>
2024-10-12 12:20:48 +0800

    feat: enhance user management via webhook events
    - Add new webhook route handling logic in `route.ts` for creation, updating, and deletion of user data based on webhook events
    - Introduce handling of various user actions and related types in `shared.types.d.ts`
    - Implement user creation, update, and deletion functions in `user.action.ts` using new type definitions
    - Add the `svix` library to dependencies in `package.json` to facilitate webhook handling
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,9 +2,15 @@
 
 import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
+import {
+  CreateUserParams,
+  UpdateUserParams,
+  DeleteUserParams,
+} from './shared.types'
+import { revalidatePath } from 'next/cache'
+import Question from '@/database/question.model'
 
 export async function getUserById(params: any) {
-  // eslint-disable-next-line no-empty
   try {
     connectToDatabase()
 
@@ -13,6 +19,63 @@ export async function getUserById(params: any) {
     const user = await User.findOne({ clerkId: userId })
 
     return user
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase()
+
+    const newUser = await User.create(userData)
+
+    return newUser
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId, updateData, path } = params
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    })
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   '_id'
+    // )
+
+    await Question.deleteMany({ author: user._id })
+
+    const deletedUser = await User.findByIdAndDelete(user._id)
+
+    return deletedUser
   } catch (error) {
     console.log(error)
     throw error
