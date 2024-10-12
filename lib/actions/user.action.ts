d84bfff81e0d98cee62694da7777c8c4c8f704c3
jim.chen <azx011014@gmail.com>
2024-10-13 07:48:24 +0800

    feat: refactor app for saved questions feature
    - Replace homepage filters and content with saved questions layout and filters.
    - Implement user authentication and display content based on saved questions for logged-in users.
    - Update "No questions to show" message to reflect saved questions context.
    - Change data type of upvotes from numeric to array in `QuestionCard`.
    - Compute upvotes using the length of the array.
    - Refactor actions related to user data: modify imports and add `getSavedQuestions` function to include search and sorting functionality.
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import User from '@/database/user.model'
+import { FilterQuery } from 'mongoose'
 import { connectToDatabase } from '../mongoose'
 import {
   CreateUserParams,
@@ -8,9 +8,12 @@ import {
   DeleteUserParams,
   GetAllUsersParams,
   ToggleSaveQuestionParams,
+  GetSavedQuestionsParams,
 } from './shared.types'
 import { revalidatePath } from 'next/cache'
 import Question from '@/database/question.model'
+import Tag from '@/database/tag.model'
+import User from '@/database/user.model'
 
 export async function getUserById(params: any) {
   try {
@@ -130,6 +133,41 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
     }
 
     revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId, searchQuery } = params
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+      : {}
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: 'saved',
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' },
+      ],
+    })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    const savedQuestions = user.saved
+
+    return { questions: savedQuestions }
   } catch (error) {
     console.log(error)
     throw error
