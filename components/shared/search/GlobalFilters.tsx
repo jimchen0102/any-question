d36302d6304192ff666b5a9a4abf5d30169fa91b
jim.chen <azx011014@gmail.com>
2024-10-13 22:16:36 +0800

    feat: implement global search enhancements and components
    - Add new `GlobalFilters` component file
    - Add new `GlobalResult` component with static sample data and result mapping logic
    - Enhance `GlobalSearch` by incorporating search debounce and dynamic loading of search results
    - Include Radix UI icons package in `package.json` dependencies
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

---
+++ components/shared/search/GlobalFilters.tsx
@@ -0,0 +1,7 @@
+const GlobalFilters = () => {
+  return (
+    <p className="text-dark400_light900 paragraph-semibold px-5">Filters</p>
+  )
+}
+
+export default GlobalFilters
--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,47 @@
+'use client'
+
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
+import { useState, useEffect } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
+import GlobalResult from './GlobalResult'
 
 const GlobalSearch = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const searchParams = useSearchParams()
+  const query = searchParams.get('global')
+  const [search, setSearch] = useState(query || '')
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'global',
+          value: search,
+        })
+
+        router.push(newUrl, { scroll: false })
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['global', 'type'],
+          })
+
+          router.push(newUrl, { scroll: false })
+        }
+      }
+    }, 500)
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [search, router, pathname, searchParams, query])
+
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -16,9 +56,17 @@ const GlobalSearch = () => {
         <Input
           type="text"
           placeholder="Search globally"
+          value={search}
           className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+          onChange={(e) => {
+            setSearch(e.target.value)
+
+            if (!isOpen) setIsOpen(true)
+            if (e.target.value === '' && isOpen) setIsOpen(false)
+          }}
         />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   )
 }
