c0dd29db6aa443c797247a5c33cb8fe69b2ea67d
jim.chen <azx011014@gmail.com>
2024-10-13 12:39:35 +0800

    feat: enhance search functionality in Home component
    - Modify `Home` component to accept search parameters and use them in the `getQuestions` query
    - Add type definitions for `SearchParamsProps`
    - Enhance the `getQuestions` function in `question.action.ts` to support search functionality based on query parameters including title and content regex matching
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,12 +15,24 @@ import {
   QuestionVoteParams,
 } from './shared.types'
 import { revalidatePath } from 'next/cache'
+import { FilterQuery } from 'mongoose'
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase()
 
-    const questions = await Question.find({})
+    const { searchQuery } = params
+
+    const query: FilterQuery<typeof Question> = {}
+
+    if (searchQuery) {
+      query.$or = [
+        { title: { $regex: new RegExp(searchQuery, 'i') } },
+        { content: { $regex: new RegExp(searchQuery, 'i') } },
+      ]
+    }
+
+    const questions = await Question.find(query)
       .populate({
         path: 'tags',
         model: Tag,
